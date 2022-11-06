aa18891d8adef0e8c3af8baf0750a0bd1956d30a
Gideon Appau <gideonad.codez@gmail.com>
2022-11-06 11:39:19 +0000

    First Commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const search = document.querySelector(".search");
+
+search.addEventListener("submit", (e) => {
+  e.preventDefault();
+  document.querySelector(".container").style.display = "block";
+  const input = document.getElementById("input").value;
+  const URL = `https://api.github.com/users/${input}`;
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      document.querySelector(".details").style.display = "none"
+      document.querySelector(".user").innerHTML = data.name;
+      document.querySelector(".username").innerHTML = "@" + data.login;
+      document.querySelector(".location").innerHTML =  data.location;
+      document.querySelector(".image").setAttribute("src", data.avatar_url);
+      document.querySelector(".joined").innerHTML = "Joined " + new Date(data.created_at);
+      document.querySelector(".bio").innerHTML = data.bio || "Oops, this user does not have an about!!";
+      document.getElementById("repo").innerHTML = data.public_repos;
+      document.getElementById("followers").innerHTML = data.followers;
+      document.getElementById("following").innerHTML = data.following;
+      document.querySelector(".profile-link").setAttribute("href", data.html_url);
+      document.querySelector(".twitter-link").setAttribute("href", `https://twitter.com/${data.twitter_username}`);
+      if(data.twitter_username === null){
+         document.querySelector(".twitter").style.display = "none";
+      }else{
+         document.querySelector(".twitter").style.display = "block";
+      }
+      input.length = 0;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
