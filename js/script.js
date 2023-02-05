22577d309c6c0d116bf9ea3d684f6c624a8c0078
Gideon Appau <gideonad.codez@gmail.com>
2023-02-05 12:33:31 -0800

    init commit

--- js/script.js
+++ js/script.js
@@ -14,16 +14,21 @@ fetchData = () => {
           { value: 1e9, symbol: "G" },
           { value: 1e12, symbol: "T" },
           { value: 1e15, symbol: "P" },
-          { value: 1e18, symbol: "E" }
-        ]
-        
-        const rx = /\.0+$|(\.[0-9]*[1-9])0+$/
-        const item = lookup.slice().reverse().find((item) => {
-          return num >= item.value
-        })
-        return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0"
-      }
-      
+          { value: 1e18, symbol: "E" },
+        ];
+
+        const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+        const item = lookup
+          .slice()
+          .reverse()
+          .find((item) => {
+            return num >= item.value;
+          });
+        return item
+          ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol
+          : "0";
+      };
+
       const {
         name,
         login,
